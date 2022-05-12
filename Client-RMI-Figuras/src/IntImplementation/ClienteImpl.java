5e90b131f983eadb5267cb94de9b75681da76eae
DBS <up190588@alumnos.upa.edu.mx>
2022-05-11 23:49:28 -0500

    Figuras

---
+++ Client-RMI-Figuras/src/IntImplementation/ClienteImpl.java
@@ -0,0 +1,30 @@
+package IntImplementation;
+
+import interfaces.Shapes;
+import objects.TriangleObj;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class ClienteImpl {
+    public void connectServer() {
+        try {
+            TriangleObj triangulo;
+            float perimeter;
+            float area;
+
+            Registry registro = LocateRegistry.getRegistry("127.0.0.1", 9090);
+            Shapes interfaz = (Shapes) registro.lookup("RMIFiguras");
+            triangulo = (TriangleObj) interfaz.getTriangulo(3, 3, 3, 4);
+            perimeter = (float) triangulo.getLado1() + triangulo.getLado2() + triangulo.getLado3();
+            area = (float) (triangulo.getLado1() * triangulo.getAltura()) / 2;
+            System.out.println("Perimetro: " + perimeter);
+            System.out.println("Area: " + area);
+
+        } catch (RemoteException | NotBoundException ex) {
+            System.out.println("error: " + ex.getMessage());
+        }
+    }
+}
